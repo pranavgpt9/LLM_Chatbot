8a81b51f281f5c07f3bfbd7bb590e4af613927a1
pranav gupta <guptap12@uni.coventry.ac.uk>
2025-02-26 19:09:02 +0000

    Files and Folder structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI Project',
+    version= '0.0.0',
+    email='pranavgpt9@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
